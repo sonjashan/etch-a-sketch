1d8563f6a396615732f32fc1151206401d9779c6
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-03 11:19:38 -0500

    add a 16x16 grid: 16 row divs (each 16 cell divs)

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const containerEl = document.querySelector(".container");
+for (let i = 0; i < 16; i++) {
+    const rowDivEl = document.createElement("div");
+    rowDivEl.classList.add("row");
+    rowDivEl.textContent = i;
+    containerEl.appendChild(rowDivEl);
+    for (let j = 0; j < 16; j++) {
+        const cellDivEl = document.createElement("div");
+        cellDivEl.classList.add("cell");
+        cellDivEl.textContent = j;
+        rowDivEl.appendChild(cellDivEl);
+    }
+}
+
