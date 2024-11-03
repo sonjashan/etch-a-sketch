779baa2f7f628eb891b952d7b3568124eca790ae
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-03 13:21:21 -0500

    use flexbox to form a grid instead of row divs

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 
 const containerEl = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
-    const rowDivEl = document.createElement("div");
-    rowDivEl.classList.add("row");
-    rowDivEl.textContent = i;
-    containerEl.appendChild(rowDivEl);
+    // const rowDivEl = document.createElement("div");
+    // rowDivEl.classList.add("row");
+    // rowDivEl.textContent = i;
+    // containerEl.appendChild(rowDivEl);
     for (let j = 0; j < 16; j++) {
         const cellDivEl = document.createElement("div");
         cellDivEl.classList.add("cell");
-        cellDivEl.textContent = j;
-        rowDivEl.appendChild(cellDivEl);
+        cellDivEl.textContent = `${i}, ${j}`;
+        containerEl.appendChild(cellDivEl);
     }
 }
 
