bba78b2394ac94f1799ab3532a0fe6dfdacabae9
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-04 15:11:27 -0500

    add colour mode and clear board buttons

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const mainEl = document.querySelector("main");
-const btnEl = document.createElement("button");
-btnEl.textContent = "New Board";
-mainEl.appendChild(btnEl);
 
 function drawBoard(rowNum, colNum) {
     const containerEl = document.createElement("div");
@@ -29,12 +26,12 @@ Please input a number between 1 and 100.`));
     return size;
 }
 
-btnEl.addEventListener("click", function () {
-    let rowSize = getSize(true);
-    let colSize = getSize(false);
-    document.querySelector(".container").remove();
-    drawBoard(rowSize, colSize);
-});
+// newBtnEl.addEventListener("click", function () {
+//     let rowSize = getSize(true);
+//     let colSize = getSize(false);
+//     document.querySelector(".container").remove();
+//     drawBoard(rowSize, colSize);
+// });
 
 
 drawBoard(16, 16);
