bb9666b21694b2238e08e4e6e57a5ddb97ebc7ab
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-03 21:12:32 -0500

    Dynamically draw new board base on user input

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ const btnEl = document.createElement("button");
 btnEl.textContent = "New Board";
 mainEl.appendChild(btnEl);
 
-const containerEl = document.createElement("div");
-containerEl.classList.add("container");
-mainEl.appendChild(containerEl);
+function drawBoard(rowNum, colNum) {
+    const containerEl = document.createElement("div");
+    containerEl.classList.add("container");
+    mainEl.appendChild(containerEl);
 
-for (let i = 0; i < 16; i++) {
-    // const rowDivEl = document.createElement("div");
-    // rowDivEl.classList.add("row");
-    // rowDivEl.textContent = i;
-    // containerEl.appendChild(rowDivEl);
-    for (let j = 0; j < 16; j++) {
-        const cellDivEl = document.createElement("div");
-        cellDivEl.classList.add("cell");
-        cellDivEl.textContent = `${i}, ${j}`;
-        containerEl.appendChild(cellDivEl);
+    for (let i = 0; i < rowNum; i++) {
+        for (let j = 0; j < colNum; j++) {
+            const cellDivEl = document.createElement("div");
+            cellDivEl.classList.add("cell");
+            cellDivEl.style.width = `${100 / colNum}%`;
+            containerEl.appendChild(cellDivEl);
+        }
     }
 }
 
+function getSize(ifRow) {
+    let size;
+    do {
+        size = Number(prompt(`How many ${ifRow ? 'ROWs' : 'COLUMNs'} for the new board?
+Please input a number between 1 and 100.`));
+    } while (Number.isNaN(size) || size < 1 || size > 100);
+    return size;
+}
+
+btnEl.addEventListener("click", function () {
+    let rowSize = getSize(true);
+    let colSize = getSize(false);
+    document.querySelector(".container").remove();
+    drawBoard(rowSize, colSize);
+});
+
+drawBoard(16, 16);
